02a2779ce2bef3be204cb13de2b701be2fa9b147
rockyjia <jrk1987@gmail.com>
2017-01-02 15:35:44 +0800

    extra excise 8

--- app/controllers/account/posts_controller.rb
+++ app/controllers/account/posts_controller.rb
@@ -1,6 +1,20 @@
 class Account::PostsController < ApplicationController
   before_action :authenticate_user!
+
   def index
     @posts = current_user.posts
   end
+
+  def edit
+    @post = Post.find(post.group,post)
+    redirect_to group_post_path
+  end
+
+  def destroy
+    @post = Post.destroy
+    redirect_to group_post_path, alert: "You have no permission."
+
+  end
+
+
 end
